a14b7014a3fb6d93661a0807725645b3072c1c32
KT147 <kevin.tammm@gmail.com>
2025-09-26 18:25:02 +0300

    fetch() functsiooni üle kirjutamine

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,13 @@ import Async from "./Async";
 
 describe('Async component', () => {
     test('renders posts if request succeeds', async () => {
+        
+        // kirjutab test funktsioonis üle fetch functioni
+        window.fetch = jest.fn()
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{id: 'p1', title: 'First post'}]
+        })
+
         render(<Async />)
 
         const listItemElements = await screen.findAllByRole('listitem', {exact: true}, {timeout:1000})
